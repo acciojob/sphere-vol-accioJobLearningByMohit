d991772284ebf2f6a8574c6592aeb17d7886fb74
accioJobLearningByMohit <mohitzod56062@gmail.com>
2026-04-07 16:19:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function volume_sphere() {
-    //Write your code here
-  
+    
+  let radius = document.getElementById("radius").value;
+	 // Convert to number
+    radius = Number(radius);
+
+	let valume=document.getElementById("valume");
+	 // Calculate volume
+    var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+	 // Calculate volume
+    var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+
+    // Set result
+    document.getElementById("volume").value = volume.toFixed(2);
+
+    // Prevent form submission
+    return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
